5422751bde20505b1210473cb4a664959b5c6159
João V. Santos <joaovaledosantos@gmail.com>
2024-02-21 21:37:57 -0300

    [Aula-20] Criando uma Int Array

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        int[] valores = {10, 20, 15, 30, 40};
+        Arrays.sort(valores);
+        System.out.println(Arrays.toString(valores));
+        System.out.println(valores.length);
+    }
+}
