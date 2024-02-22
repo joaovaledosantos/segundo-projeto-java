8bdc4231c051bc7ba5b8f378b031547606509322
João V. Santos <joaovaledosantos@gmail.com>
2024-02-22 01:41:56 -0300

    [Aula-23] Aplicando constantes

---
+++ src/aula3.java
@@ -0,0 +1,8 @@
+public class aula3 {
+    public static void main(String[] args) {
+        final double taxaJuros = 11.92;
+        System.out.println(taxaJuros);
+        //taxaJuros = 11.52;
+        System.out.println(taxaJuros);
+    }
+}
