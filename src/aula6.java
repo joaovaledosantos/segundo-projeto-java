2984065f5909011121134e1149bc4d61e2ef648a
João V. Santos <joaovaledosantos@gmail.com>
2024-02-22 08:20:14 -0300

    [Aula-26] Convertendo String em Integers

---
+++ src/aula6.java
@@ -0,0 +1,7 @@
+public class aula6 {
+    public static void main(String[] args) {
+        String valorProduto = "10";
+        int valorTotal = Integer.parseInt(valorProduto) + 2;
+        System.out.println(valorProduto);
+    }
+}
