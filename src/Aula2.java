88073ce49b8126c9504ff36c9d6900acac1c6ce1
João V. Santos <joaovaledosantos@gmail.com>
2024-02-21 22:12:02 -0300

    [Aula-22] Arrays Multidimensionais

--- src/Aula2.java
+++ src/Aula2.java
@@ -2,9 +2,8 @@ import java.util.Arrays;
 
 public class Aula2 {
     public static void main(String[] args) {
-        String[] cars = {"BMW", "tesla", "Jeep", "Fiat"};
-        cars[2] = "Ferrari";
-        System.out.println(Arrays.toString(cars));
-        System.out.println(cars[2]);;
+        String[][] cars = {{"BMW", "tesla"},{"Jeep", "Fiat"}};
+        System.out.println(Arrays.deepToString(cars));
+
     }
 }
