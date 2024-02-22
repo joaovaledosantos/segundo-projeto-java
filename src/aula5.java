15c701388396b2bc02ed91d414b97b8ff99327df
João V. Santos <joaovaledosantos@gmail.com>
2024-02-22 07:56:39 -0300

    [Aula-25]  Ordem dos Operadores

---
+++ src/aula5.java
@@ -0,0 +1,8 @@
+public class aula5 {
+    public static void main(String[] args) {
+        int a = 6 + 2 * 2;
+        int b = 6 + 2 * (2 -3);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
